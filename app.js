685bd5aac2a564a6122473f017e2cdb66f3076af
Raj Kumar <rajkumar.cind@gmail.com>
2018-05-06 16:32:45 +0530

    more-perfection

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+//Intital Setup for Node js 
 var express    = require("express"),
     bodyParser = require("body-parser"),
 	http       = require('http'),
 	request = require('request'),
-    cheerio = require('cheerio'),
+	cheerio = require('cheerio'),
+	Crawler = require("node-webcrawler"),
     URL = require('url-parse'),
     app        = express();
 
@@ -10,43 +12,56 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-
+//=============================================
+//				INITIAL SETUP OVER
+//=============================================
 app.get("/", function(req, res){
 	res.render("index");
 });
 app.post("/submit", function(req, res){
 	//Just Some Initial Work 
 	var sizeDisplay;
+	var retrySize;
 	var linkToSameDomain = [];
 	//Checking if it is a Valid url
 	var pageToVisit = req.body.URLvalue;
 	//Actual Computation
 	var url = new URL(pageToVisit);
-	console.log(url);
-	// var firstDot = UrlSize.indexOf(".");
-	// var dotCuttedUrl = UrlSize.slice(firstDot, UrlSize.length);
-	//Now to Remove the First Slash in dotCuttedUrl
-	// var firstSlash = dotCuttedUrl.indexOf("/");
-	// var pathURl = dotCuttedUrl.slice(firstSlash, dotCuttedUrl.length);
-	// var hostURL = dotCuttedUrl.slice(0, firstSlash);
-	// console.log(hostURL);
-	// console.log(pathURl);
-	// console.log(firstSlash);
-	//starting Page
-	var req = http.request({ 
-	    host: url.host,
-	    port: 80,
-	    path: url.pathname,
-	    method: 'HEAD' }, 
-	    function(res) {
-			console.log("Size: " + res.headers['content-length']);
-			sizeDisplay = res.headers['content-length'];
+	//console.log(url);
+	var c = new Crawler({
+		maxConnections : 10,
+		// This will be called for each crawled page
+		callback : function (error, result, $) {
+			if(error){
+				console.log(error);
+			}else{
+				console.log("==================================");				
+				console.log(result.request.responseContent. bytesRead);
+				retrySize = result.request.responseContent. bytesRead;
+				console.log("==================================");
+			}
+		}
+	});
+	c.queue(pageToVisit);
+	
+	// var req = http.request({ 
+	//     host: url.host,
+	//     port: 80,
+	//     path: url.pathname,
+	//     method: 'HEAD' }, 
+	//     function(res) {
+	// 		console.log("Size: " + res.headers['content-length']);
+	// 		sizeDisplay = res.headers['content-length'];
+	// 		//console.log(res.headers);
 			
-	    }
-	).end();
-	//Determing the Size of Web Page Kinda Over!
-	// ======================================= //
-	//To Determine the number of links pointing to the same Domain
+	//     }
+	// ).end();
+
+	
+// ============================================================ //
+//			Determing the Size of Web Page Kinda Over!
+//To Determine the number of links pointing to the same Domain
+// ============================================================ //	
 	request(pageToVisit, function(error, response, body) {
 		var allAbsoluteLinks = [];
 		
@@ -85,12 +100,15 @@ app.post("/submit", function(req, res){
 	
 	//Redering the Result to the result page
 	setTimeout(function(){
-		console.log(sizeDisplay);
-		res.render("result", {urlSize: sizeDisplay, linkToSameDomain: linkToSameDomain});
-	},500)
+		//console.log(sizeDisplay);
+		res.render("result", {urlSize: retrySize, linkToSameDomain: linkToSameDomain});
+	},5000)
 	
 });
 
+//======================================
+//	ENV SETUP, TURNING UP THE SERVER
+//======================================
 app.listen(4400, function(){
 	console.log("Server running at http://localhost:4400/");
 });
